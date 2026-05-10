c3f4632b57b5926f0bd5a441ffc9dc596ebccc3e
Dralen <oginnisayofunmi12@gmail.com>
2026-05-10 14:50:14 +0100

    removed vercel.json

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // in development, use the local server; in production, use the same origin
 // because in production there is no localhost 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : `${import.meta.env.VITE_API_BASE_URL ?? ""}/api`;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
